1d1861e1eabff201da14f0f97fbd22e532be6d47
GameBeastYT <75262920+GameBeastYT@users.noreply.github.com>
2021-05-11 08:07:52 +0530

    Add files via upload

---
+++ components/bottomTabNavigator.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, Alert, Modal, ScrollView, KeyboardAvoidingView } from 'react-native';
+import firebase from 'firebase'
+import db from '../config'
+import {createBottomTabNavigator} from 'react-navigation-tabs'
+import exchangeScreen from '../screens/exchangeScreen'
+import homeScreen from '../screens/homeScreen'
+
+export const BottomTabNavigator = createBottomTabNavigator({
+    home: {screen:homeScreen},
+    exchange: {screen:exchangeScreen}
+})
